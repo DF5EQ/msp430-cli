657406e2415062f43ec703e35c3de965402ee8b4
Peter Bägel (DF5EQ) <peter@baegel.de>
2022-09-21 22:57:42 +0200

    empty command files created

---
+++ command/command.c
@@ -0,0 +1,25 @@
+/* ===== file header ===== */
+
+/* ===== includes ===== */
+#include <msp430.h>
+#include "command.h"
+
+/* ===== private datatypes ===== */
+
+/* ===== private symbols ===== */
+
+/* ===== private constants ===== */
+
+/* ===== public constants ===== */
+
+/* ===== private variables ===== */
+
+/* ===== public variables ===== */
+
+/* ===== private functions ===== */
+
+/* ===== public functions ===== */
+void command_init (void)
+{
+}
+
---
+++ command/command.h
@@ -0,0 +1,20 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+
+/* ===== file header ===== */
+
+/* ===== includes ===== */
+
+/* ===== public datatypes ===== */
+
+/* ===== public symbols ===== */
+
+/* ===== public constants ===== */
+
+/* ===== public variables ===== */
+
+/* ===== public functions ===== */
+void command_init (void);
+
+#endif
+
