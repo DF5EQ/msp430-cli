96e28d46b5ff4a22fceb26cbd4cde4d5410690b8
Peter Bägel (DF5EQ) <peter@baegel.de>
2022-12-25 00:12:40 +0100

    empty files for terminal function added

---
+++ terminal/terminal.c
@@ -0,0 +1,35 @@
+/* ===== file header ===== */
+
+/* ===== includes ===== */
+#include "terminal.h"
+
+/* ===== private datatypes ===== */
+
+/* ===== private symbols ===== */
+
+/* ===== private constants ===== */
+
+/* ===== public constants ===== */
+
+/* ===== private variables ===== */
+
+/* ===== public variables ===== */
+
+/* ===== private functions ===== */
+
+/* ===== public functions ===== */
+
+void terminal_init (void)
+{
+}
+
+int terminal_putc (char c)
+{
+    return 0;
+}
+
+int terminal_gets (char* s)
+{
+    return 0;
+}
+
